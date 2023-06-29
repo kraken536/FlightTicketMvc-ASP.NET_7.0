5e01c070da6a43364c97d06a0d9b6e6a3c5e30a0
Kraken536 <abd.aa536@gmail.com>
2023-06-29 10:55:26 +0300

    Some tables regarding the flight details were added.

---
+++ Controllers/FlightController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightTicketsReservationMVC.Controllers
+{
+    public class FlightController : Controller
+    {
+        public IActionResult SeatDetails()
+        {
+            return View();
+        }
+    }
+}
